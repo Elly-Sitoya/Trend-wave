dd1ccd11747b5358fbd7e87c0b85715b1800ae50
Elly Sitoya <ellykymz254@gmail.com>
2023-12-30 20:48:12 +0300

    added routes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,28 @@
+import { Link } from "react-router-dom";
+import logo from "../images/avatar4.jpg";
+import { CiMenuBurger } from "react-icons/ci";
+import { AiOutlineClose } from "react-icons/ai";
+
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <nav>
+      <div className="container nav-container">
+        <Link to={"/"}>
+          <img src={logo} alt="navbar logo" />
+        </Link>
+        <ul className="nav-menu">
+          <li>
+            <Link to={"/profile"}>Ernest Achiever</Link>
+            <Link to={"/create"}>create post</Link>
+            <Link to={"/authors"}>authors</Link>
+            <Link to={"/logout"}>logout</Link>
+          </li>
+        </ul>
+        <button className="nav_toggle-btn">
+          <CiMenuBurger />
+        </button>
+      </div>
+    </nav>
+  );
 };
 export default Header;
--- src/index.js
+++ src/index.js
@@ -4,10 +4,40 @@ import "./index.css";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Layout from "./components/Layout";
 import ErrorPage from "./pages/ErrorPage";
+import Home from "./pages/Home";
+import PostDetail from "./pages/PostDetail";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import UserProfile from "./pages/UserProfile";
+import Authors from "./pages/Author";
+import CreatePost from "./pages/CreatePost";
+import EditPost from "./pages/EditPost";
+import CategoryPost from "./pages/CategoryPost";
+import AuthorPosts from "./pages/AuthorPosts";
+import Dashboard from "./pages/Dashboard";
+import Logout from "./pages/Logout";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 const router = createBrowserRouter([
-  { path: "/", element: <Layout />, errorElement: <ErrorPage /> },
+  {
+    path: "/",
+    element: <Layout />,
+    errorElement: <ErrorPage />,
+    children: [
+      { index: true, element: <Home /> },
+      { path: "/post/:id", element: <PostDetail /> },
+      { path: "register", element: <Register /> },
+      { path: "login", element: <Login /> },
+      { path: "profile/:id", element: <UserProfile /> },
+      { path: "authors", element: <Authors /> },
+      { path: "create", element: <CreatePost /> },
+      { path: "posts/categories/:category", element: <CategoryPost /> },
+      { path: "posts/users/:id", element: <AuthorPosts /> },
+      { path: "myposts/:id", element: <Dashboard /> },
+      { path: "posts/:id/edit", element: <EditPost /> },
+      { path: "logout", element: <Logout /> },
+    ],
+  },
 ]);
 root.render(
   <React.StrictMode>
